e017735f1dd78f7cf5fa3dd7188fb70692edb5da
operah24 <66219854+operah24@users.noreply.github.com>
2021-03-19 16:38:04 +0100

    implemented backend test

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,5 @@
 const { registerUser, login} = require("./user");
 const {uploadFile, getFileById} = require("./file");
-const compare = require("./compare");
-const result = require("./result");
+const {compare} = require("./compare");
 
-module.exports = { registerUser, login, uploadFile, getFileById, compare, result }
+module.exports = { registerUser, login, uploadFile, getFileById, compare }
